45e4109fead002f65f6c53dda1d2cf60f8806f2f
oahmaro <osamaahmaro@gmail.com>
2019-04-29 14:18:01 +0300

    Created app views

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 // Index Route
 app.get('/', (req, res) => {
-  res.send('Index')
+  const title = 'Welcome1' 
+  res.render('index', {title})
 })
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('About')
+  res.render('about')
 })
 
 const port = 5000;
