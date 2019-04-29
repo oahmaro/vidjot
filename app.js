6afa3f89b947a546d18fbe47aa4c0c4768652f29
oahmaro <osamaahmaro@gmail.com>
2019-04-29 16:54:31 +0300

    Installed & Connected Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 const app = express();
+
+// Connect to mongoose
+mongoose.connect("mongodb+srv://osama:Osama12@vidjot-sntzu.mongodb.net/test?retryWrites=true", {
+  useNewUrlParser: true
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
