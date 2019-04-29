c1f13aa70dec00dd7bc6c31dd89f465140c3a22b
oahmaro <osamaahmaro@gmail.com>
2019-04-29 11:13:19 +0300

    Create two basic routes

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Index')
+})
+
+// About Route
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
 const port = 5000;
 
 app.listen(port, () => {
