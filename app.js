996419fdc834f0d62c5569e7e35937578464d86d
oahmaro <osamaahmaro@gmail.com>
2019-04-29 20:14:33 +0300

    Add Idea Form

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
 
+// App Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add')
+})
+
 const port = 5000;
 
 app.listen(port, () => {
