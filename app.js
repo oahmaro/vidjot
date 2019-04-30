64407fa23a38f10a9f8df9a44a22b6ef411be69f
oahmaro <osamaahmaro@gmail.com>
2019-04-30 13:29:25 +0300

    Idea edit form

--- app.js
+++ app.js
@@ -46,6 +46,15 @@ app.get('/ideas/add', (req, res) => {
   res.render('ideas/add')
 })
 
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render('ideas/edit', {idea})
+  })
+});
+
 // Process Form
 app.post('/ideas', (req, res) => {
   let errors = [];
