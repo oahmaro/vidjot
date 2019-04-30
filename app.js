6ca6782643da14633fc01fa6e596e4bb1eefccc3
oahmaro <osamaahmaro@gmail.com>
2019-04-30 10:27:29 +0300

    Fetching Ideas from MongoDB

--- app.js
+++ app.js
@@ -34,7 +34,14 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
 
-// App Idea Form
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({}).sort({date: 'desc'}).then(ideas => {
+    res.render('ideas/index', {ideas})
+  })
+})
+
+// Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add')
 })
